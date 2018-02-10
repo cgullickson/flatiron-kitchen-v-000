72d6b9f3c812378d86e176c03e88815ce4567596
Chris Gullickson <cgullickson5@gmail.com>
2018-02-10 00:00:24 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,25 @@
 class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new 
+  end
+
+  def create 
+  end
+
+  def edit 
+  end 
+
+  def update 
+  end 
+
+  def show 
+  end 
+
+  private
+
+  def ingredient_params 
+  end
 end
