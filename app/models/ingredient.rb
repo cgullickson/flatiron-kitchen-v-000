4c3ab05df9fa3f111a35eec1c30e8b9c7cb19a06
Chris Gullickson <cgullickson5@gmail.com>
2018-02-10 00:10:08 +0000

    automatically backed up by learn

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ActiveRecord::Base
-  belongs_to :recipe
+  validates :name, presence: true
   has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+  accepts_nested_attributes_for :ingredients
 end
